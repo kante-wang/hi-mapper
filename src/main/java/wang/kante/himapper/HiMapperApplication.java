6e6108d87d06d881d0b9c8da1e6b083d43acc815
kante <wangchuanwei@rtkj.com.cn>
2018-09-13 15:01:01 +0800

    提交项目

---
+++ src/main/java/wang/kante/himapper/HiMapperApplication.java
@@ -0,0 +1,12 @@
+package wang.kante.himapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiMapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiMapperApplication.class, args);
+	}
+}
